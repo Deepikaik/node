b0a6e2fbc8589dd59e2487b4384a541a5cebae52
Deepikaik <deepikabhagavati2001@gmail.com>
2024-09-23 12:30:45 +0530

    'java'

--- CRUD js/one/app.js
+++ CRUD js/one/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
 
 import router from './router.js'
 
@@ -11,6 +12,13 @@ app.use(morgan('tiny'))
 dotenv.config({'path':"./test.config"})
 let port=process.env.PORT
 let hostname=process.env.HOSTNAME
+
+//read from data
+//app.use(express.json())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))   //for add operations
+// parse application/json
+app.use(bodyParser.json())
 
 app.get("/",(req,resp)=>{
     resp.send("root request")
--- CRUD js/one/router.js
+++ CRUD js/one/router.js
@@ -3,29 +3,81 @@ import fs from 'fs'
 
 let router=express.Router()
 
-router.post("/create",(req,resp)=>{
-    let emp=req.body
-});
-
 router.get("/read", async (req,resp)=>{
     let employees = await getEmployees()
     return resp.json(employees)
-});
+})
+
+router.post("/create",async(req,resp)=>{
+    let employee=req.body
+    let employees=await getEmployees()
+    let emp_data= employees.find((emp)=>{
+        return emp.eid== employee.eid;
+    })
+    console.log(emp_data)
+    if(emp_data){
+        return resp.status(401).json({"msg":"employee already exists"})
+    }
+    employees.push(employee)
+    saveemployees(employees)
+    return resp.status(200).json({"msg":"new employee created"})
+    
+})
+
+
 
 /* router.get("/read",async(req,resp)=>{
     let employees=await getEmployees()
     return resp.json(employees)
 }) */
-router.put("/update/:id",(req,resp)=>{
-    let eid=req.params.id
+router.put("/update/:eid",async(req,resp)=>{
+    let emp_id=req.params.id
+    let employee=req.body;
+    let employees=await getEmployees();
+
+    let emp= employees.find((emp)=>{
+        return emp.eid==emp_id
+    })
+
+if(!emp){
+    return resp.status(401).json({"msg":"employee not exists"})  //return makes it come out of the block
+}
+
+let remaining_employees=employees.filter((emp)=>{
+    emp.eid!=emp_id
 })
-router.delete("/del/:id",(req,resp)=>{
+remaining_employees.unshift(employee)
+saveemployees(remaining_employees)
+return resp.status(208).json({"msg":"employee obj updated"})
+})
+
+
+
+
+
+router.delete("/del/:id",async(req,resp)=>{
     let eid=req.params.id;
+    let employees= await getEmployees();
+    let emp_data=employees.find(()=>{
+        return emp.eid==emp_id
+    })
+    if(!emp_data){
+        return resp.status(401).json({"msg":"employee doesnt exist"})
+    }
+    let remaining_employees=employees.filter(()=>{
+        return emp.eid!=emp_id
+    })
+    saveemployees(remaining_employees)
+    return resp.status(208).json({"msg":"deleted successfully"})
 })
 
 let getEmployees=()=>{
     let emp_data=fs.readFileSync("data.json",'utf-8')
-    return json.parse(emp_data)
+    return JSON.parse(emp_data)
+}
+
+let saveemployees = (employees)=>{
+    fs.writeFileSync('data.json',JSON.stringify(employees))
 }
 
 export default router;
